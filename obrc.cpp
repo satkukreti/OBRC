73468e003c895095146f65335e9a7c7bd0251fba
Satvik Kukreti <skukret1@cs.binghamton.edu>
2024-01-30 16:25:09 -0500

    Added program timing functionalities

--- obrc.cpp
+++ obrc.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <chrono>
+
 using namespace std;
 
 int main() {
+  //Keeping track of start time
+  chrono::time_point<chrono::system_clock> start, end;
+  start = chrono::system_clock::now();
+  
   cout << "Hello World!" << endl;
+
+  //End time
+  end = chrono::system_clock::now();
+  chrono::duration<double> elapsed_seconds = end - start;
+  cout << "Elapsed time: " << elapsed_seconds.count() << "s" << endl;
+
+
   return 0;
 }
