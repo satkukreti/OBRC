d4d56fdd001d188adfea87ca1418f7a3abcb6372
Satvik Kukreti <skukret1@cs.binghamton.edu>
2024-01-30 19:06:49 -0500

    Using mmap, finished line by line parsing

--- obrc.cpp
+++ obrc.cpp
@@ -1,14 +1,20 @@
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #include <iostream>
 #include <chrono>
-#include <sstream>
 #include <fstream>
-#include <string>
+#include <cstring>
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
   if(argc != 3){
-    cerr << "Error: <input_file> <output_file>" << endl;
+    cerr << "Error: ./obrc <input_file> <output_file>" << endl;
     exit(1);
   }
   
@@ -16,23 +22,30 @@ int main(int argc, char* argv[]) {
   chrono::time_point<chrono::system_clock> start, end;
   start = chrono::system_clock::now();
 
-  string fname = argv[1];
-  ifstream inFile;
-  inFile.open(fname);
+  char* fname = argv[1];
+  int fd = open(fname, O_RDONLY, S_IRUSR | S_IWUSR);
+  struct stat sb;
 
-  if(!inFile) {
-    cerr << "Error: file cannot be opened" << endl;
+  if(fstat(fd, &sb) == -1){
+    cerr << "Error: could not get file size" << endl;
     exit(1);
   }
 
-  string line;
-  while(getline(inFile, line)){
-    size_t pos = line.rfind(';');
-    string cityName = line.substr(0, pos);
-    double cityTemp = stod(line.substr(pos+1));
-    cout << cityName << endl;
-    cout << cityTemp << endl;
-    
+  //allocating memory for the input file
+  char* filemem = static_cast<char *>(mmap(nullptr, sb.st_size, PROT_READ, MAP_PRIVATE, fd, 0));
+
+  
+  //Get file input, line by line
+  size_t currentPos = 0;
+  while (currentPos < static_cast<size_t>(sb.st_size)) {
+    const char* line = filemem + currentPos;
+    size_t lineLength = strcspn(line, "\n");
+
+    cout.write(line, lineLength);
+    cout << endl;
+
+    //moves on to the next line
+    currentPos += lineLength + 1;
   }
 
   /*FILE* fp = fopen(argv[2], "w");
@@ -45,7 +58,8 @@ int main(int argc, char* argv[]) {
   chrono::duration<double> elapsed_seconds = end - start;
   cout << "Elapsed time: " << elapsed_seconds.count() << "s" << endl;
 
-  inFile.close();
+  munmap(filemem, sb.st_size);
+  close(fd);
 
   return 0;
 }
